c5e227b56d5a3a6e1892482f9c7896221d51c4df
GamagamiDev <81793604+GamagamiDev@users.noreply.github.com>
2022-09-11 23:56:30 -0500

    Added Prototype 2

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+
+    private float leftBound = -14;
+    private float rightBound = 14;
+    private float spawnPosZ = 20;
+
+    private void Start()
+    {
+        //InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+    }
+
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            SpawnRandomPrefab();
+
+            float randomDelay = Random.Range(0.8f, 2.0f);
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+    }
+
+    void SpawnRandomPrefab()
+    {
+        int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+    }
+}
